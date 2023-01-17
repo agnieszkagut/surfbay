77664d459b34c5c36776b70964d5a8a7b8e2fa43
MINGW64 <Amelia@MSI>
2023-01-17 16:33:01 +0100

    my 1st commit

---
+++ src/app/pricesPage/prices/prices.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-prices',
+  standalone: true,
+  templateUrl: './prices.component.html',
+  styleUrls: ['./prices.component.css']
+})
+export class PricesComponent {
+
+}
