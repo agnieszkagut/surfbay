d6dc9339e831ae25e8e5a7836824e8eddaed6072
MINGW64 <Amelia@MSI>
2023-01-17 20:43:01 +0100

    jsY

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterLink, RouterLinkActive, RouterModule, Routes } from '@angular/router';
 
-import { AppComponent } from './app.component';
-import { FooterComponent } from './componentFooter/footer/footer.component';
+import { AppComponent } from './app.component.js';
+import { FooterComponent } from './componentFooter/footer/footer.component.js';
 // import { HeaderModule } from './componentSurf/header/header.module';
-import { HomeComponent } from './homePage/home/home.component';
-import { AboutComponent } from './aboutPage/about/about.component';
-import { SurfComponent } from './surfPage/surf/surf.component';
-import { PricesComponent } from './pricesPage/prices/prices.component';
-import { ContactComponent } from './contactPage/contact/contact.component';
-import { HeaderComponent } from './componentSurf/header/header.component';
+import { HomeComponent } from './homePage/home/home.component.js';
+import { AboutComponent } from './aboutPage/about/about.component.js';
+import { SurfComponent } from './surfPage/surf/surf.component.js';
+import { PricesComponent } from './pricesPage/prices/prices.component.js';
+import { ContactComponent } from './contactPage/contact/contact.component.js';
+import { HeaderComponent } from './componentSurf/header/header.component.js';
 import { CommonModule, NgFor } from '@angular/common';
 
 const routes: Routes = [
