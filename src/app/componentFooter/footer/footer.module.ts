56acebd41ffdfc5e5da6a6508d25fdb6529580d9
MINGW64 <Amelia@MSI>
2023-01-17 17:21:48 +0100

    Revert "my 1st commit"
    This reverts commit 77664d459b34c5c36776b70964d5a8a7b8e2fa43.

--- src/app/componentFooter/footer/footer.module.ts
+++
@@ -1,9 +0,0 @@
-import { NgModule } from '@angular/core';
-//import { FooterComponent } from './footer.component';
-
-@NgModule({
-  declarations: [FooterComponent],
-  providers: [],
-  exports: [FooterComponent]
-})
-export class FooterComponent { }
