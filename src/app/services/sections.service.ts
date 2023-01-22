51a01042882e1cd5790849fa82aad8200e2b26b7
MINGW64 <Amelia@MSI>
2023-01-22 21:45:48 +0100

    photos ok

--- src/app/services/sections.service.ts
+++ src/app/services/sections.service.ts
@@ -13,25 +13,25 @@ export class SectionsService {
       id:1,
       sectionName:"surfing lessons",
       sectionDesc:"We will teach you surfing whether you are beginner or more experienced",
-      sectionImg:"../../../assets/img/11.jpg"
+      sectionImg:"./assets/img/11.jpg"
     },
     {
       id:2,
       sectionName:"surf guiding",
       sectionDesc:"We will show you the best spots to surf depends on weather in Taghazout, Tamraght, Agadir and more",
-      sectionImg:"../../../assets/img/22.jpg"
+      sectionImg:"./assets/img/22.jpg"
     },
     {
       id:3,
       sectionName:"sandboarding",
       sectionDesc:"Another fun way to use a surfboard is boarding on a desert and we are here to show you how",
-      sectionImg:"../../../assets/img/33.jpg"
+      sectionImg:"./assets/img/33.jpg"
     },
     {
       id:4,
       sectionName:"more activities",
       sectionDesc:"With us you don't miss local attractions like Paradise Valley with beautiful natural pools or riding on moroccan camel",
-      sectionImg:"../../../assets/img/44.jpg"
+      sectionImg:"./assets/img/44.jpg"
     }
     ];
 
@@ -40,18 +40,18 @@ export class SectionsService {
         id:1,
         sectionName:"experience",
         sectionDesc:"As couches we have experienced surf instruction for years. You will be provided with top-notch instruction, equipment, and accommodations",
-        sectionImg:"../../../assets/img/aboutus4.jpg"
+        sectionImg:"./assets/img/aboutus4.jpg"
       },      {
         id:2,
         sectionName:"solid training and feedback",
         sectionDesc:"Chcouches will offer tailored lessons to help you make the most of your time. You will get expert guidance and feedbacks",
-        sectionImg:"../../../assets/img/aboutus.jpg"
+        sectionImg:"./assets/img/aboutus.jpg"
       },
       {
         id:3,
         sectionName:"making it fun",
         sectionDesc:"We represent patient and passionate about sharing the joys of surfing with others while immersing you in the beautiful moroccan coastal waves and the surf culture",
-        sectionImg:"../../../assets/img/aboutus2.jpg"
+        sectionImg:"./assets/img/aboutus2.jpg"
       }
     ]
   
